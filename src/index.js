4a14ee65b9f0ea5804a8002b254d07f7d1d3f7e2
Jeel <viradiyajeel@gmail.com>
2024-04-09 23:03:49 +0530

    added utils

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import {app} from "./app.js"
 
 dotenv.config({
     path : "./env"
 })
 
+const port = process.env.PORT || 7000
+
 connectDB()
+.then(() => {
+    app.listen(port, () => {
+        console.log(`Server is listening on port ${port}`);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection failed!! ", error);
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+//using Promises
+
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((error) => next(error))
+    }
+}
+
+
+export {asyncHandler}
+
+//using try-catch method
+
+/* 
+const asyncHandler = (requestHandler) => {
+    async (req, res, next) => {
+        try {
+            await requestHandler(req,res,next)
+        } catch (error) {
+            res.status(error.code() || 500).json({
+                success: false,
+                message: error.message
+            })
+        }
+    }
+}
+
+*/
+
+export {requestHandler}
